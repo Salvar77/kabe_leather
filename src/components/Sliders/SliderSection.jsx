e9a8d5515d565a454b9f70095660504879fa65d0
Łukasz <lukaszkus77@gmail.com>
2023-12-30 21:34:06 +0100

    update

--- src/components/Sliders/SliderSection.jsx
+++ src/components/Sliders/SliderSection.jsx
@@ -3,8 +3,7 @@ import BeforeImage1 from "../../assets/image/renovation_2.jpg";
 import AfterImage1 from "../../assets/image/renovation_1.jpg";
 import BeforeImage2 from "../../assets/image/renovation_4.jpg";
 import AfterImage2 from "../../assets/image/renovation_3.jpg";
-import BeforeImage3 from "../../assets/image/renovation_6.jpg";
-import AfterImage3 from "../../assets/image/renovation_5.jpg";
+
 import classes from "./SliderSection.module.scss";
 
 const SliderSection = (props) => {
@@ -14,7 +13,6 @@ const SliderSection = (props) => {
       <div className={classes.sliderGrid}>
         <SliderTrue beforeImage={BeforeImage1} afterImage={AfterImage1} />
         <SliderTrue beforeImage={BeforeImage2} afterImage={AfterImage2} />
-        <SliderTrue beforeImage={BeforeImage3} afterImage={AfterImage3} />
       </div>
     </section>
   );
