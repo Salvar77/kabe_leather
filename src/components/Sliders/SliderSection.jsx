f5dba5fb8ef734022416193019da9c0976976b4c
Łukasz <lukaszkus77@gmail.com>
2024-01-02 17:47:32 +0100

    update

--- src/components/Sliders/SliderSection.jsx
+++ src/components/Sliders/SliderSection.jsx
@@ -3,8 +3,7 @@ import BeforeImage1 from "../../assets/image/renovation_2.jpg";
 import AfterImage1 from "../../assets/image/renovation_1.jpg";
 import BeforeImage2 from "../../assets/image/renovation_4.jpg";
 import AfterImage2 from "../../assets/image/renovation_3.jpg";
-import BeforeImage3 from "../../assets/image/renovation_6.jpg";
-import AfterImage3 from "../../assets/image/renovation_5.jpg";
+
 import classes from "./SliderSection.module.scss";
 import Link from "next/link";
 
@@ -15,7 +14,6 @@ const SliderSection = (props) => {
       <div className={classes.sliderGrid}>
         <SliderTrue beforeImage={BeforeImage1} afterImage={AfterImage1} />
         <SliderTrue beforeImage={BeforeImage2} afterImage={AfterImage2} />
-        <SliderTrue beforeImage={BeforeImage3} afterImage={AfterImage3} />
       </div>
       <div className={classes.buttonContainer}>
         <Link href="/realizacje" className={classes.realizationButton}>
