81529767e37f3d4f89c606269393b1204f31aa27
Łukasz <lukaszkus77@gmail.com>
2024-01-10 13:55:00 +0100

    update

--- src/components/Nav/Logo.jsx
+++ src/components/Nav/Logo.jsx
@@ -1,10 +1,17 @@
 import classes from "./Logo.module.scss";
 import Image from "next/image";
-import logo from "../../assets/image/logo5.png";
+import logo from "../../assets/image/logo_true6.png";
 
 const Logo = () => {
   return (
-    <Image className={classes.logo} src={logo} alt="Logo Firmy" priority />
+    <Image
+      className={classes.logo}
+      src={logo}
+      alt="Logo Firmy"
+      priority
+      width={110}
+      height={110}
+    />
   );
 };
 export default Logo;
