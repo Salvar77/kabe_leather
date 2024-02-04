036314b2c14fc430a4966d827c79ad904af19b82
Łukasz <lukaszkus77@gmail.com>
2024-02-04 18:48:15 +0100

    update

--- src/pages/Realizations/Realizations.jsx
+++ src/pages/Realizations/Realizations.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import SliderTrue from "../../components/Sliders/SliderTrue"; // Upewnij się, że ścieżka jest poprawna
+import SliderSection from "../../components/Sliders/SliderSection"; // Upewnij się, że ścieżka jest poprawna
 import classes from "./Realizations.module.scss";
-import SliderSection from "../../components/Sliders/SliderSection";
+import sliderClasses from "../../components/Sliders/SliderSection.module.scss"; // Upewnij się, że ścieżka jest poprawna
+import BeforeImage1 from "../../assets/image/renovation_7.jpg";
+import AfterImage1 from "../../assets/image/renovation_8.jpg";
+import BeforeImage2 from "../../assets/image/renovation_4.jpg";
+import AfterImage2 from "../../assets/image/renovation_3.jpg";
+import BeforeImage3 from "../../assets/image/renovation_6.png";
+import AfterImage3 from "../../assets/image/renovation_5.png";
+import BeforeImage4 from "../../assets/image/renovation_8-1.jpg";
+import AfterImage4 from "../../assets/image/renovation_7-1.jpg";
+import BeforeImage5 from "../../assets/image/renovation_9.jpg";
+import AfterImage5 from "../../assets/image/renovation_9-1.jpg";
+// Załóżmy, że te importy obrazów są poprawne
 import realizationsPhoto from "../../assets/image/realizations.jpg";
 import realizationsPhoto2 from "../../assets/image/realizations2.jpg";
 import realizationsPhoto3 from "../../assets/image/realizations3.jpg";
@@ -9,14 +22,16 @@ import realizationsPhoto4 from "../../assets/image/realizations4.jpg";
 
 const Realizations = () => {
   return (
-    <section id="#realizations" className={classes.realizations}>
+    <section className={`${classes.realizations} realizationsPage`}>
       <h1 className={classes.realizations__header}>Realizacje</h1>
 
-      <SliderSection
-        showTitle={false}
-        showButton={false}
-        customBgColor={true}
-      />
+      <div className={classes.slidersContainer}>
+        <SliderTrue beforeImage={BeforeImage1} afterImage={AfterImage1} />
+        <SliderTrue beforeImage={BeforeImage2} afterImage={AfterImage2} />
+        <SliderTrue beforeImage={BeforeImage3} afterImage={AfterImage3} />
+        <SliderTrue beforeImage={BeforeImage4} afterImage={AfterImage4} />
+        <SliderTrue beforeImage={BeforeImage5} afterImage={AfterImage5} />
+      </div>
 
       <div className={classes.extraImages}>
         <Image
@@ -31,12 +46,12 @@ const Realizations = () => {
         />
         <Image
           src={realizationsPhoto3}
-          alt="Opis obrazu 2"
+          alt="Opis obrazu 3"
           className={classes.image}
         />
         <Image
           src={realizationsPhoto4}
-          alt="Opis obrazu 2"
+          alt="Opis obrazu 4"
           className={classes.image}
         />
       </div>
