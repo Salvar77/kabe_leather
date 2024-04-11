fd4bad010db9ed70d672cf8e9f60879cd3822112
Łukasz <lukaszkus77@gmail.com>
2024-04-11 18:06:19 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,32 @@ import "@/styles/globals.scss";
 import "hamburgers/dist/hamburgers.min.css";
 import Nav from "@/components/Nav/Nav";
 import BurgerMenu from "@/components/Nav/BurgerMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "@/components/Nav/Logo";
 import Footer from "@/components/Footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
+  const [showLogo, setShowLogo] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos =
+      window.pageYOffset || document.documentElement.scrollTop;
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 992) {
+      setShowLogo(currentScrollPos <= 200);
+    } else {
+      setShowLogo(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleNav = () => {
@@ -18,7 +37,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <header>
-        <Logo />
+        {showLogo && <Logo />}
         <Nav isOpen={isOpen} toggleNav={toggleNav} />
         <BurgerMenu handleOpen={toggleNav} isOpen={isOpen} />
       </header>
