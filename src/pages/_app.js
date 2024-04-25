ef283e5159c0e4a15e1e93f3b25a147a4efe861b
Łukasz <lukaszkus77@gmail.com>
2024-04-25 13:17:59 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }) {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleNav = () => {
