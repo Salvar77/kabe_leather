59a24fb9d675f7a1012797815e50910216e9c4d2
Łukasz <lukaszkus77@gmail.com>
2024-07-03 18:32:42 +0200

    update

--- scripts/GenerateSitemap.js
+++ scripts/GenerateSitemap.js
@@ -2,18 +2,31 @@ const fs = require("fs");
 const path = require("path");
 
 // Tutaj zdefiniuj swoje statyczne i dynamiczne ścieżki
-const paths = ["/", "/AboutUs", "/contact"];
+const paths = [
+  "/",
+  "/O-Mnie",
+  "/Tapicerka/tapicerka-samochodowa",
+  "/Tapicerka/tapicerka-domowa",
+  "/Tapicerka/bezinwazyjne-przyciemnianie-szyb",
+  "/Realizacje",
+  "/pranie-tapicerki-samochodowej", // Nowa ścieżka
+];
 
-// Generowanie sitemap.xml
 const GenerateSitemap = () => {
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${paths
         .map((path) => {
           return `
             <url>
               <loc>${`https://www.kabetintleather.pl${path}`}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <priority>0.80</priority>
             </url>
           `;
         })
@@ -21,7 +34,7 @@ const GenerateSitemap = () => {
     </urlset>
   `;
 
-  fs.writeFileSync("public/sitemap.xml", sitemap); // Zapisz sitemapę w folderze public
+  fs.writeFileSync(path.join(__dirname, "../public/sitemap.xml"), sitemap); // Zapisz sitemapę w folderze public
 };
 
 GenerateSitemap();
