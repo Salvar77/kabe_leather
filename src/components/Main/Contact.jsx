2f6e2bf487cd17ff3a37195606ea8452deef4035
Łukasz <lukaszkus77@gmail.com>
2024-07-15 15:14:14 +0200

    update

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -11,9 +11,17 @@ const Contact = () => {
 
   const [messageStatus, setMessageStatus] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [consent, setConsent] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!consent) {
+      setMessageStatus(
+        "Musisz wyrazić zgodę na przetwarzanie danych osobowych."
+      );
+      setShowModal(true);
+      return;
+    }
     try {
       const response = await fetch("/api/send", {
         method: "POST",
@@ -49,77 +57,105 @@ const Contact = () => {
   };
 
   return (
-    <section id="kontakt" className={classes.contactSection}>
-      <div className={classes.contactInfo}>
-        <h2>Kontakt</h2>
-        <p>
-          Ulica:{" "}
-          <span className={classes.highlightedText}>Grota Roweckiego 18</span>
-        </p>
-        <p>Miasto: Opole</p>
-        <p>
-          Telefon:{" "}
-          <span className={`${classes.highlightedText} noLinkStyle`}>
-            881 325 631
-          </span>
-        </p>
-        <p>E-mail: kabetint@gmail.com</p>
-      </div>
-      <form className={classes.contactForm} onSubmit={handleSubmit}>
-        <h2>Wyślij nam wiadomość</h2>
-        <input
-          type="text"
-          name="name"
-          placeholder="Twoje imię"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Twój email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <textarea
-          type="message"
-          name="message"
-          placeholder="Twoja wiadomość"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        ></textarea>
-        <button type="submit">Wyślij</button>
-      </form>
-
-      <div className={classes.linkWrapper}>
-        <Link href="tel:+48881325631" className={classes.appointmentLink}>
-          Umów się na wizytę!
-        </Link>
-      </div>
-
-      {showModal && (
-        <div className={classes.modal} onClick={closeModal}>
-          <div className={classes.modalContent}>
-            {messageStatus && <p>{messageStatus}</p>}
-          </div>
+    <>
+      <section id="kontakt" className={classes.contactSection}>
+        <div className={classes.contactInfo}>
+          <h2>Kontakt</h2>
+          <p>
+            Ulica:{" "}
+            <span className={classes.highlightedText}>Grota Roweckiego 18</span>
+          </p>
+          <p>Miasto: Opole</p>
+          <p>
+            Telefon:{" "}
+            <span className={`${classes.highlightedText} noLinkStyle`}>
+              881 325 631
+            </span>
+          </p>
+          <p>E-mail: kabetint@gmail.com</p>
         </div>
-      )}
-      <div className={classes.mapWrapper}>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.1219679260016!2d17.944938899999997!3d50.680561000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471053a4ba7c88cb%3A0x78814edaf226a49a!2sGrota-Roweckiego%2018%2C%2045-268%20Opole!5e0!3m2!1spl!2spl!4v1702909803946!5m2!1spl!2spl"
-          title="Mapa lokalizacji firmy"
-          width="100%"
-          height="300"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
-    </section>
+        <form className={classes.contactForm} onSubmit={handleSubmit}>
+          <h2>Wyślij nam wiadomość</h2>
+          <input
+            type="text"
+            name="name"
+            placeholder="Twoje imię"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Twój email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <textarea
+            type="message"
+            name="message"
+            placeholder="Twoja wiadomość"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+          <div className={classes.consentWrapper}>
+            <input
+              type="checkbox"
+              id="rodoConsent"
+              checked={consent}
+              onChange={() => setConsent(!consent)}
+              required
+            />
+            <label htmlFor="rodoConsent">
+              Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z
+              ustawą o ochronie danych osobowych w związku z wysłaniem zapytania
+              przez formularz kontaktowy. Podanie danych jest dobrowolne, ale
+              niezbędne do przetworzenia zapytania. Zostałem poinformowany, że
+              przysługuje mi prawo dostępu do swoich danych, możliwość ich
+              poprawienia oraz żądanie zaprzestania ich przetwarzania.
+              Administratorem danych osobowych jest KaBe Tint & Leather Kamil
+              Brzoskwinia, ul. Grota Roweckiego 18.
+            </label>
+          </div>
+          <button type="submit">Wyślij</button>
+        </form>
+
+        <div className={classes.linkWrapper}>
+          <Link href="tel:+48881325631" className={classes.appointmentLink}>
+            Umów się na wizytę!
+          </Link>
+        </div>
+
+        {showModal && (
+          <div className={classes.modal} onClick={closeModal}>
+            <div className={classes.modalContent}>
+              {messageStatus && <p>{messageStatus}</p>}
+            </div>
+          </div>
+        )}
+        <div className={classes.mapWrapper}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.1219679260016!2d17.944938899999997!3d50.680561000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471053a4ba7c88cb%3A0x78814edaf226a49a!2sGrota-Roweckiego%2018%2C%2045-268%20Opole!5e0!3m2!1spl!2spl!4v1702909803946!5m2!1spl!2spl"
+            title="Mapa lokalizacji firmy"
+            width="100%"
+            height="300"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </section>
+      <p className={classes.policy}>
+        Więcej informacji na temat przetwarzania danych osobowych znajdziesz w
+        naszej{" "}
+        <Link href="/polityka-prywatnosci" className={classes.policy__link}>
+          Polityce Prywatności
+        </Link>
+      </p>
+    </>
   );
 };
 
