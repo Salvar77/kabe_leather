e2581a12e59438964c22e70c9a97bb97a1c973ee
Łukasz <lukaszkus77@gmail.com>
2024-07-22 18:14:42 +0200

    update

--- src/components/Nav/Logo.jsx
+++ src/components/Nav/Logo.jsx
@@ -6,8 +6,8 @@ import logo from "../../assets/image/logo_true6.png";
 const Logo = ({ showLogo }) => {
   const logoStyle = {
     opacity: showLogo ? 1 : 0,
-    transform: showLogo ? "translateY(0)" : "translateY(-20px)", // Zwiększony przesuw dla lepszego efektu
-    transition: "opacity 0.5s ease, transform 0.5s ease", // Dodaj przejście bezpośrednio tutaj
+    transform: showLogo ? "translateY(0)" : "translateY(-20px)",
+    transition: "opacity 0.5s ease, transform 0.5s ease",
   };
 
   return (
