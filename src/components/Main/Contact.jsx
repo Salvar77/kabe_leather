b1564faafcba3cb6ce94a213f9b155f501083d88
Łukasz <lukaszkus77@gmail.com>
2024-07-22 16:23:32 +0200

    update

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -134,8 +134,7 @@ const Contact = () => {
         )}
         <div className={classes.mapWrapper}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.1219679260016!2d17.944938899999997!3d50.680561000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471053a4ba7c88cb%3A0x78814edaf226a49a!2sGrota-Roweckiego%2018%2C%2045-268%20Opole!5e0!3m2!1spl!2spl!4v1702909803946!5m2!1spl!2spl"
-            title="Mapa lokalizacji firmy"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.123817301448!2d17.942779077017107!3d50.68052667077051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471053a4a6619f2b%3A0x24aa7034946c9c12!2sGrota-Roweckiego%2017%2C%2046-020%20Opole!5e0!3m2!1spl!2spl!4v1721653941236!5m2!1spl!2spl"
             width="100%"
             height="300"
             style={{ border: 0 }}
