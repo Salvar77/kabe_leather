aee177760fbac2d04cec68d75c075c0466f1a48f
Łukasz <lukaszkus77@gmail.com>
2024-08-15 21:30:55 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,28 @@ import "@/styles/globals.scss";
 import "hamburgers/dist/hamburgers.min.css";
 import Nav from "@/components/Nav/Nav";
 import BurgerMenu from "@/components/Nav/BurgerMenu";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import Logo from "@/components/Nav/Logo";
 import Footer from "@/components/Footer/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [showLogo, setShowLogo] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = document.documentElement.scrollTop;
+      const windowWidth = window.innerWidth;
+
+      setShowLogo(!(windowWidth < 992 && currentScrollPos > 200));
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
   const toggleNav = () => {
     setIsOpen(!isOpen);
@@ -20,6 +34,7 @@ export default function App({ Component, pageProps }) {
       <header>
         <Nav isOpen={isOpen} toggleNav={toggleNav} />
         <BurgerMenu handleOpen={toggleNav} isOpen={isOpen} />
+        <Logo showLogo={showLogo} />
       </header>
 
       <Component {...pageProps} />
