25b457228934a23f45154d4ad7fb17ac79274a1a
Łukasz <lukaszkus77@gmail.com>
2024-11-08 09:51:10 +0100

    update

--- src/components/Main/SEO.jsx
+++ src/components/Main/SEO.jsx
@@ -8,51 +8,55 @@ const SEO = ({ title, description, image }) => (
     <meta property="og:description" content={description} />
     <meta property="og:image" content={image} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script type="application/ld+json">{`
-        {
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
           "@context": "https://schema.org",
           "@type": "LocalBusiness",
-          "url": "https://www.kabetintleather.pl",
-          "name": "Kabe Tint&Leather Auto Detailing",
-          "description": "Profesjonalne pranie tapicerki, czyszczenie tapicerki oraz czyszczenie samochodu w Opolu. Skontaktuj się z nami, aby zadbać o czystość swojego pojazdu.",
-          "logo": "https://www.kabetintleather.pl/kabelogooo.webp",
-          "telephone": "+48-881-325-631",
-          "contactPoint": {
+          url: "https://www.kabetintleather.pl",
+          name: "Kabe Tint&Leather Auto Detailing",
+          description:
+            "Profesjonalne pranie tapicerki, czyszczenie tapicerki oraz czyszczenie samochodu w Opolu. Skontaktuj się z nami, aby zadbać o czystość swojego pojazdu.",
+          logo: "https://www.kabetintleather.pl/kabelogooo.webp",
+          telephone: "+48-881-325-631",
+          contactPoint: {
             "@type": "ContactPoint",
-            "telephone": "+48-881-325-631",
-            "contactType": "Customer Support",
-            "areaServed": "Poland",
-            "availableLanguage": ["Polish", "English"]
+            telephone: "+48-881-325-631",
+            contactType: "Customer Support",
+            areaServed: "Poland",
+            availableLanguage: ["Polish", "English"],
           },
-          "address": {
+          address: {
             "@type": "PostalAddress",
-            "streetAddress": "ul. Generała Emila Fieldorfa 12",
-            "addressLocality": "Opole",
-            "postalCode": "45-273",
-            "addressCountry": "PL"
+            streetAddress: "ul. Generała Emila Fieldorfa 12",
+            addressLocality: "Opole",
+            postalCode: "45-273",
+            addressCountry: "PL",
           },
-          "geo": {
+          geo: {
             "@type": "GeoCoordinates",
-            "latitude": "50.675106",
-            "longitude": "17.921298"
+            latitude: "50.675106",
+            longitude: "17.921298",
           },
-          "openingHours": ["Mo,Tu,We,Th,Fr 07:00-20:00", "Sa 08:00-18:00"],
-          "priceRange": "PLN",
-          "sameAs": [
+          openingHours: ["Mo,Tu,We,Th,Fr 07:00-20:00", "Sa 08:00-18:00"],
+          priceRange: "PLN",
+          sameAs: [
             "https://www.facebook.com/profile.php?id=61554800660887",
-            "https://www.instagram.com/kabe_tintleather_autodetailing?igsh=MWdwYzAzdmM2c2Rlbg=="
+            "https://www.instagram.com/kabe_tintleather_autodetailing?igsh=MWdwYzAzdmM2c2Rlbg==",
           ],
-          "serviceType": [
+          serviceType: [
             "Pranie tapicerki",
             "Czyszczenie samochodu",
-            "Czyszczenie tapicerki"
+            "Czyszczenie tapicerki",
           ],
-          "areaServed": {
+          areaServed: {
             "@type": "Place",
-            "name": "Opole, Poland"
-          }
-        }
-      `}</script>
+            name: "Opole, Poland",
+          },
+        }),
+      }}
+    />
   </Head>
 );
 
