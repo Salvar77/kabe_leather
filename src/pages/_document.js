dc7acf1cd0b12b91d8ecaccaca28b06bece56a53
Łukasz <lukaszkus77@gmail.com>
2024-11-26 18:25:54 +0100

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,16 +35,20 @@ class MyDocument extends Document {
 
           {/* Google Tag Manager */}
           <Script
-            id="gtm"
+            id="gtm-script"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NC377D3W');
-              `,
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[]; 
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'}); 
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:''; 
+                j.async=true; 
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NC377D3W');
+            `,
             }}
           />
 
