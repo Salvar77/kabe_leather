a4f0967aae6784b0470460745b088d69b2253f6e
Łukasz <lukaszkus77@gmail.com>
2024-11-27 15:44:42 +0100

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,102 +1,90 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script
+          {/* Cookiebot */}
+          <Script
             id="Cookiebot"
             src="https://consent.cookiebot.com/uc.js"
-            data-cbid={process.env.COOKIEBOT_SECRET_KEY}
+            data-cbid={process.env.NEXT_PUBLIC_COOKIEBOT_SECRET_KEY}
             data-blockingmode="auto"
-            type="text/javascript"
-          ></script>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            strategy="lazyOnload"
+            async
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
           {/* Google Tag Manager */}
-          <script
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NC377D3W');
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[];
+                  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                  var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                  j.async=true;
+                  j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
+                  f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
               `,
             }}
           />
-          {/* End Google Tag Manager */}
-          {/* Google Analytics Tag */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-16652411588"
-          ></script>
-          <script
+
+          {/* Google Analytics */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="lazyOnload"
+          />
+          <Script
+            id="google-analytics"
+            strategy="lazyOnload"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-
-                gtag('config', 'AW-16652411588');
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
               `,
             }}
           />
-          {/* End Google Analytics Tag */}
-          {/* Google Ads Tag */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-PFL7MVJ5ZE"
-          ></script>
-          <script
+
+          {/* Google Ads */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
+            strategy="lazyOnload"
+          />
+          <Script
+            id="google-ads"
+            strategy="lazyOnload"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-
-                gtag('config', 'G-PFL7MVJ5ZE');
-                gtag('config', 'AW-16608400370'); // New Google Ads Conversion Tag
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}');
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_AW_ID}');
               `,
             }}
           />
-          {/* End Google Ads Tag */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NC377D3W"
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
           </noscript>
-          {/* Google Tag Manager (noscript) */}
+
           <Main />
           <NextScript />
-          {/* Event snippet for Kontakt conversion page */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                gtag('event', 'conversion', {'send_to': 'AW-16608400370/9avKCNyvktMZEPKnwO89'});
-              `,
-            }}
-          />
-          {/* End Google Ads Event Snippet */}
         </body>
       </Html>
     );
