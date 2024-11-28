8a6b611ab247692cd812722c06a063411e96a0a0
Łukasz <lukaszkus77@gmail.com>
2024-11-28 20:40:11 +0100

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,60 +6,14 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Cookiebot */}
-          <Script
-            id="Cookiebot"
-            src="https://consent.cookiebot.com/uc.js"
-            data-cbid={process.env.NEXT_PUBLIC_COOKIEBOT_SECRET_KEY}
-            data-blockingmode="auto"
-            strategy="lazyOnload"
-            async
-          />
-
           {/* Preconnect & Preload */}
 
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            async
           />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
-          {/* Google Tag Manager */}
-          <Script
-            id="gtm-script"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];
-                  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;
-                  j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-              `,
-            }}
-          />
-
-          {/* Google Analytics */}
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-            strategy="lazyOnload"
-          />
-          <Script
-            id="google-analytics"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-              `,
-            }}
-          />
 
           {/* Google Ads */}
           <Script
@@ -81,16 +35,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          {/* Google Tag Manager (noscript fallback) */}
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-
           <Main />
           <NextScript />
         </body>
