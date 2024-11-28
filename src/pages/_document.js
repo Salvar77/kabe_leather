c72196570b1d537dd0d3cec54b1210a231009190
Łukasz <lukaszkus77@gmail.com>
2024-11-28 22:34:40 +0100

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* Preconnect & Preload */}
-
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
@@ -27,9 +26,46 @@ class MyDocument extends Document {
                   var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
                   j.async=true;
-                  j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
+                  j.src='https://www.googletagmanager.com/gtm.js?id=GTM-NC377D3W'+dl;
                   f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+                })(window,document,'script','dataLayer','GTM-NC377D3W');
+              `,
+            }}
+          />
+
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16652411588"
+            strategy="lazyOnload"
+          />
+          <Script
+            id="google-analytics"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16652411588');
+              `,
+            }}
+          />
+
+          {/* Google Ads */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-PFL7MVJ5ZE"
+            strategy="lazyOnload"
+          />
+          <Script
+            id="google-ads"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-PFL7MVJ5ZE');
+                gtag('config', 'AW-16608400370');
               `,
             }}
           />
@@ -38,7 +74,7 @@ class MyDocument extends Document {
           {/* Google Tag Manager (noscript fallback) */}
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NC377D3W"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
