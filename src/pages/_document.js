0febf8109d154f7a506d1d6dae70253eff55370e
Łukasz <lukaszkus77@gmail.com>
2024-11-29 00:24:18 +0100

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,80 +7,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* Preconnect & Preload */}
+
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
             async
           />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
-          {/* Google Tag Manager */}
-          <Script
-            id="gtm-script"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];
-                  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;
-                  j.src='https://www.googletagmanager.com/gtm.js?id=GTM-NC377D3W'+dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NC377D3W');
-              `,
-            }}
-          />
-
-          {/* Google Analytics */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=AW-16652411588"
-            strategy="lazyOnload"
-          />
-          <Script
-            id="google-analytics"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-16652411588');
-              `,
-            }}
-          />
-
-          {/* Google Ads */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-PFL7MVJ5ZE"
-            strategy="lazyOnload"
-          />
-          <Script
-            id="google-ads"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-PFL7MVJ5ZE');
-                gtag('config', 'AW-16608400370');
-              `,
-            }}
-          />
         </Head>
         <body>
-          {/* Google Tag Manager (noscript fallback) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NC377D3W"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-
           <Main />
           <NextScript />
         </body>
