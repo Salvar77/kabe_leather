9bc1f3fd9f06076b58ac1cca14027834586dbb4d
Łukasz <lukaszkus77@gmail.com>
2025-09-21 16:47:10 +0200

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import classes from "./Footer.module.scss";
 import ContactMain from "../More/ContactMain";
-import GoogleMap from "../More/GoogleMap";
 import Link from "next/link";
+import dynamic from "next/dynamic"; // 1. Importowanie 'dynamic'
+
+// 2. Dynamiczne ładowanie komponentu GoogleMap
+// Wskazówka: ssr: false jest kluczowe, ponieważ komponent używa 'window', które jest dostępne tylko w przeglądarce.
+// loading: () => ... to opcjonalny stan ładowania.
+const DynamicGoogleMap = dynamic(() => import("../More/GoogleMap"), {
+  ssr: false,
+  loading: () => <p>Ładowanie mapy...</p>,
+});
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={classes.footer}>
-      <GoogleMap />
+      {/* 3. Użycie dynamicznie zaimportowanego komponentu */}
+      <DynamicGoogleMap />
       <ContactMain />
       <div className={classes.footer__container}>
         <div className={classes.footer__text}>
