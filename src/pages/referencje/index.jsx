3c5c22fbad9f9dc3a3b2e5f4bf8c5d25ae17696e
Łukasz <lukaszkus77@gmail.com>
2025-09-21 18:06:22 +0200

    update

--- src/pages/referencje/index.jsx
+++ src/pages/referencje/index.jsx
@@ -230,14 +230,14 @@ const Referency = () => {
                 alt={`Referencje - ${photo.alt} - pranie tapicerki, czyszczenie samochodu`}
                 className={classes.mobileImage}
                 priority={i === 0}
-                fetchPriority="high"
+                fetchPriority={i === 0 ? "high" : "auto"}
               />
               <Image
                 src={photo.largeSrc}
                 alt={`Referencje - ${photo.alt} - pranie tapicerki, czyszczenie samochodu`}
                 className={classes.desktopImage}
                 priority={i === 0}
-                fetchPriority="high"
+                fetchPriority={i === 0 ? "high" : "auto"}
               />
             </div>
           ))}
