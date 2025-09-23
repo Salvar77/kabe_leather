f4baaf47ae8074bb344f473335c3d702202fee33
Łukasz <lukaszkus77@gmail.com>
2025-09-23 19:05:39 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,8 +7,6 @@ import Logo from "@/components/Nav/Logo";
 import Footer from "@/components/Footer/Footer";
 import ContactBubble from "@/components/More/ContactBubble";
 import ContactBar from "@/components/Main/ContactBar";
-import Head from "next/head";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -33,24 +31,6 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){
-        w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-    `
-              .replace(/\s+/g, " ")
-              .trim(),
-          }}
-        />
-      </Head>
       <header>
         <ContactBar />
         <Nav isOpen={isOpen} toggleNav={toggleNav} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,23 @@ class MyDocument extends Document {
     return (
       <Html lang="pl">
         <Head>
+          <Script
+            id="gtm-script"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+  (function(w,d,s,l,i){
+    w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
+    f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+`
+                .replace(/\s+/g, " ")
+                .trim(),
+            }}
+          />
+
           <link rel="icon" href="/favicon1.ico" />
         </Head>
         <body>
