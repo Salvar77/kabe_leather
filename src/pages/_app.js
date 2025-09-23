fcc4132bca9a3e492d37ca549f85c1a34055cc8e
Łukasz <lukaszkus77@gmail.com>
2025-09-23 18:55:52 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,8 @@ import Logo from "@/components/Nav/Logo";
 import Footer from "@/components/Footer/Footer";
 import ContactBubble from "@/components/More/ContactBubble";
 import ContactBar from "@/components/Main/ContactBar";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,6 +33,24 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+    `
+              .replace(/\s+/g, " ")
+              .trim(),
+          }}
+        />
+      </Head>
       <header>
         <ContactBar />
         <Nav isOpen={isOpen} toggleNav={toggleNav} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,20 @@ import Script from "next/script";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pl">
         <Head>
           <link rel="icon" href="/favicon1.ico" />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
