b758d3316009ed6b1055630dc4a90754348ad0da
Łukasz <lukaszkus77@gmail.com>
2025-09-24 20:54:56 +0200

    update

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = ({ height = "100vh", mobileWhiteBlockColor = "#1D120C" }) => {
             objectFit="cover"
             objectPosition={isMobile ? "center" : "80% 20%"}
             priority
-            quality={65}
+            quality={75}
             sizes="(max-width: 768px) 100vw, 100vh"
           />
         </div>
